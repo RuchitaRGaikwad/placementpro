3014a427a42c6378a01ca249bf3c7ef76d9eb448
Ruchita Gaikwad <gaikwadrr02@gmail.com>
2025-10-12 17:35:07 +0000

    something different in logo

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -12,11 +12,20 @@ export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
       className={cn('text-primary', className)}
       {...props}
     >
+      <rect width="32" height="32" rx="8" fill="currentColor" />
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32ZM13.25 10H17.75C20.25 10 22 11.75 22 14.25C22 16.75 20.25 18.5 17.75 18.5H13.25V10ZM10 22V10H10.75H13.25V18.5H10.75V22H10ZM15.75 18.5V22H13.25V18.5H15.75Z"
-        fill="currentColor"
+        d="M12 18.5V9H16.5C18.9853 9 21 11.0147 21 13.5C21 15.9853 18.9853 18 16.5 18H14"
+        stroke="hsl(var(--primary-foreground))"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 23L12 18.5"
+        stroke="hsl(var(--primary-foreground))"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
