4e241ebe10210f1a508131ad95d8adfbf886f444
Ruchita Gaikwad <gaikwadrr02@gmail.com>
2025-10-12 14:28:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-295370737-6dc30",
+  "appId": "1:251962923609:web:29942304c31712ac252f48",
+  "apiKey": "AIzaSyDLCYwucJoAfuWIjq590nZYOmvXr3xRrg0",
+  "authDomain": "studio-295370737-6dc30.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "251962923609"
+};
